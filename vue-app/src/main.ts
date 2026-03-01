543d351b7225f74f522b764382dd7483b853e9d5
Michal Chalany <mikaelchalany@gmail.com>
2026-03-01 08:05:20 +0100

    feat: add default styles with open sans font

--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -1,7 +1,7 @@
-import './assets/main.css'
+import './assets/styles/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
