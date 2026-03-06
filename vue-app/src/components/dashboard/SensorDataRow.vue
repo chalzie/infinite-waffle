24f98320eaf30189fd80bd15741937d03d6a4d7f
Michal Chalany <mikaelchalany@gmail.com>
2026-03-06 09:59:26 +0100

    fix(respo): add mobile layout for sensor data row

--- vue-app/src/components/dashboard/SensorDataRow.vue
+++ vue-app/src/components/dashboard/SensorDataRow.vue
@@ -26,4 +26,31 @@ defineProps<{
   will-change: transform;
   content-visibility: auto;
 }
+
+@media (max-width: 576px) {
+  .sensor-data-row {
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: 1rem 1rem;
+    grid-template-areas:
+      'time time time value type'
+      'message device-id device-id device-id device-id';
+    margin-bottom: 1.5rem;
+  }
+
+  .sensor-data-row__time {
+    grid-area: time;
+  }
+  .sensor-data-row__value {
+    grid-area: value;
+  }
+  .sensor-data-row__message {
+    grid-area: message;
+  }
+  .sensor-data-row__device-id {
+    grid-area: device-id;
+  }
+  .sensor-data-row__type {
+    grid-area: type;
+  }
+}
 </style>
